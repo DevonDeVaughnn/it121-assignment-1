fc2aa7e6abce60fef2b2327cb0ec95105ef91fb4
Darrian DeVaughnn <devondevaughnn@gmail.com>
2021-07-07 14:21:06 -0700

    added notess

--- assignment-1.js
+++ assignment-1.js
@@ -1,11 +1,15 @@
+//creating elements for html page
 const div = document.createElement("div")
 let p = document.createElement("p")
 let w = document.createElement("p")
 p.id="welcome"
 w.id="sum"
+
+//adding elements to html page
 div.append(p,w)
 document.body.appendChild(div)
 
+//adding content to appended children
 let welcome = document.querySelector("#welcome")
 welcome.append("Welcome to my calculator")
 
