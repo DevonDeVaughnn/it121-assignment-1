77c7ba4ad399301b9621a3320e0010bb8e70b909
Darrian DeVaughnn <devondevaughnn@gmail.com>
2021-07-07 13:55:28 -0700

    uploading to heroku

---
+++ assignment-1.js
@@ -0,0 +1,8 @@
+let welcome = document.querySelector("#welcome")
+welcome.append("Welcome to my calculator")
+
+let sum = document.querySelector("#sum")
+const num1 = 5
+const num2 = 7
+result = num1 + num2
+sum.append("The sum of "+ num1 + " + " + num2 + " is: " + result )
