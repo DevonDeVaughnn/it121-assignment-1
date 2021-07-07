148668ac79402094362d9288acad269e460e81ba
Darrian DeVaughnn <devondevaughnn@gmail.com>
2021-07-07 14:18:36 -0700

    took away all html created page through js

--- assignment-1.js
+++ assignment-1.js
@@ -1,3 +1,11 @@
+const div = document.createElement("div")
+let p = document.createElement("p")
+let w = document.createElement("p")
+p.id="welcome"
+w.id="sum"
+div.append(p,w)
+document.body.appendChild(div)
+
 let welcome = document.querySelector("#welcome")
 welcome.append("Welcome to my calculator")
 
